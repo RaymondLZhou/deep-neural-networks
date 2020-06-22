a1563e8eb7b783a7ddac38294300a10548f11283
Raymond Zhou <raymondzhou2019@gmail.com>
2020-06-22 19:34:32 -0400

    Updated spacing

--- src/colourDetection/colourDetection.py
+++ src/colourDetection/colourDetection.py
@@ -2,6 +2,31 @@ import cv2
 import numpy as np
 import pandas as pd
 
+def getColorName(R,G,B):
+    minimum = 10000
+
+    for i in range(len(csv)):
+        d = abs(R- int(csv.loc[i, "R"])) + abs(G- int(csv.loc[i, "G"]))+ abs(B- int(csv.loc[i, "B"]))
+
+        if(d <= minimum):
+            minimum = d
+            cname = csv.loc[i, "color_name"]
+
+    return cname
+
+def draw_function(event, x,y,flags,param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        global b, g, r, xpos, ypos, clicked
+
+        clicked = True
+        xpos = x
+        ypos = y
+
+        b,g,r = img[y,x]
+        b = int(b)
+        g = int(g)
+        r = int(r)
+
 img = cv2.imread('../../images/colours.jpg')
 
 clicked = False
@@ -9,37 +34,17 @@ r = g = b = xpos = ypos = 0
 
 index=["color", "color_name", "hex", "R", "G", "B"]
 csv = pd.read_csv('../../data/colours.csv', names=index, header=None)
-
-def getColorName(R,G,B):
-    minimum = 10000
-    for i in range(len(csv)):
-        d = abs(R- int(csv.loc[i, "R"])) + abs(G- int(csv.loc[i, "G"]))+ abs(B- int(csv.loc[i, "B"]))
-        if(d <= minimum):
-            minimum = d
-            cname = csv.loc[i,"color_name"]
-    return cname
-
-def draw_function(event, x,y,flags,param):
-    if event == cv2.EVENT_LBUTTONDBLCLK:
-        global b, g, r, xpos, ypos, clicked
-        clicked = True
-        xpos = x
-        ypos = y
-        b,g,r = img[y,x]
-        b = int(b)
-        g = int(g)
-        r = int(r)
        
 cv2.namedWindow('image')
 cv2.setMouseCallback('image', draw_function)
 
-while(1):
+while(True):
     cv2.imshow("image", img)
     if (clicked):
    
-        cv2.rectangle(img,(20,20), (750,60), (b,g,r), -1)
+        cv2.rectangle(img, (20,20), (750,60), (b,g,r), -1)
 
-        text = getColorName(r,g,b) + ' R=' + str(r) +  ' G=' + str(g) +  ' B=' + str(b)
+        text = getColorName(r, g, b) + ' R=' + str(r) +  ' G=' + str(g) +  ' B=' + str(b)
         
         cv2.putText(img, text, (50, 50), 2, 0.8, (255,255,255), 2, cv2.LINE_AA)
 
