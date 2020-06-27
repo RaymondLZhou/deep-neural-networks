6018f20ae4e843e5c12b81ee8e711e0cf61e4cfb
Raymond Zhou <raymondzhou2019@gmail.com>
2020-06-26 23:12:43 -0400

    Created forward.py

--- src/forward.py
+++ src/forward.py
@@ -1,14 +1,35 @@
+import numpy as np
+import activate
+
+def linear_forward(A, W, b):
+    Z = np.dot(W, A) + b
+    
+    assert(Z.shape == (W.shape[0], A.shape[1]))
+    cache = (A, W, b)
+    
+    return Z, cache
+
 def linear_activation_forward(A_prev, W, b, activation):
     
     if activation == "sigmoid":
         Z, linear_cache = linear_forward(A_prev, W, b)
-        A, activation_cache = sigmoid(Z)
+        A, activation_cache = activate.sigmoid(Z)
     
     elif activation == "relu":
         Z, linear_cache = linear_forward(A_prev, W, b)
-        A, activation_cache = relu(Z)
+        A, activation_cache = activate.relu(Z)
     
     assert (A.shape == (W.shape[0], A_prev.shape[1]))
     cache = (linear_cache, activation_cache)
 
     return A, cache
+
+A_prev = np.random.randn(3,2)
+W = np.random.randn(1,3)
+b = np.random.randn(1,1)
+
+A, linear_activation_cache = linear_activation_forward(A_prev, W, b, activation = "sigmoid")
+print("With sigmoid: A = " + str(A))
+
+A, linear_activation_cache = linear_activation_forward(A_prev, W, b, activation = "relu")
+print("With ReLU: A = " + str(A))
