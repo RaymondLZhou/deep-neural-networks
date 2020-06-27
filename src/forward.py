108927e6c8a99d2db8f8de87308dacdc579096bf
Raymond Zhou <raymondzhou2019@gmail.com>
2020-06-26 23:32:59 -0400

    Updated forward.py

--- src/forward.py
+++ src/forward.py
@@ -24,12 +24,59 @@ def linear_activation_forward(A_prev, W, b, activation):
 
     return A, cache
 
-A_prev = np.random.randn(3,2)
-W = np.random.randn(1,3)
-b = np.random.randn(1,1)
+def L_model_forward(X, parameters):
+    caches = []
+    A = X
+    L = len(parameters) // 2
+    
+    for l in range(1, L):
+        A_prev = A 
+
+        A, cache = linear_activation_forward(A_prev, parameters['W' + str(l)], parameters['b' + str(l)], "relu")
+        caches.append(cache)
+        
+    AL, cache = linear_activation_forward(A, parameters['W' + str(L)], parameters['b' + str(L)], "sigmoid")
+    caches.append(cache)
+    
+    assert(AL.shape == (1, X.shape[1]))
+            
+    return AL, caches
+
+np.random.seed(1)
+A = np.random.randn(3, 2)
+W = np.random.randn(1, 3)
+b = np.random.randn(1, 1)
+
+Z, linear_cache = linear_forward(A, W, b)
+print("Z = " + str(Z))
+
+np.random.seed(2)
+A_prev = np.random.randn(3, 2)
+W = np.random.randn(1, 3)
+b = np.random.randn(1, 1)
 
 A, linear_activation_cache = linear_activation_forward(A_prev, W, b, activation = "sigmoid")
 print("With sigmoid: A = " + str(A))
 
 A, linear_activation_cache = linear_activation_forward(A_prev, W, b, activation = "relu")
 print("With ReLU: A = " + str(A))
+
+np.random.seed(6)
+X = np.random.randn(5, 4)
+W1 = np.random.randn(4, 5)
+b1 = np.random.randn(4, 1)
+W2 = np.random.randn(3, 4)
+b2 = np.random.randn(3, 1)
+W3 = np.random.randn(1, 3)
+b3 = np.random.randn(1, 1)
+
+parameters = {"W1": W1,
+              "b1": b1,
+              "W2": W2,
+              "b2": b2,
+              "W3": W3,
+              "b3": b3}
+
+AL, caches = L_model_forward(X, parameters)
+print("AL = " + str(AL))
+print("Length of caches list = " + str(len(caches)))
