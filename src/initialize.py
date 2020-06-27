bfbabfde037efe5ffdd601f3c4abdb744e559781
Raymond Zhou <raymondzhou2019@gmail.com>
2020-06-26 22:48:37 -0400

    Created initialize.py

---
+++ src/initialize.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def initialize_parameters_deep(layer_dims):
+    parameters = {}
+    L = len(layer_dims)
+
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1])*0.01
+        parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
+        
+        assert(parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[l-1]))
+        assert(parameters['b' + str(l)].shape == (layer_dims[l], 1))
+   
+    return parameters
+
+parameters = initialize_parameters_deep([5,4,3])
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
