b0dc7b32e0ea32ebdf1cdd03f2449e6e9f2ee7cb
Raymond Zhou <raymondzhou2019@gmail.com>
2020-06-29 20:45:13 -0400

    Added initialization

--- src/deepNetwork.py
+++ src/deepNetwork.py
@@ -20,4 +20,56 @@ def load_dataset():
     
     return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
 
+def convert_to_one_hot(Y, C):
+    Y = np.eye(C)[Y.reshape(-1)].T
+    return Y
+
 X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
+
+X_train_flatten = X_train_orig.reshape(X_train_orig.shape[0], -1).T
+X_test_flatten = X_test_orig.reshape(X_test_orig.shape[0], -1).T
+
+X_train = X_train_flatten/255.
+X_test = X_test_flatten/255.
+
+Y_train = convert_to_one_hot(Y_train_orig, 6)
+Y_test = convert_to_one_hot(Y_test_orig, 6)
+
+def create_placeholders(n_x, n_y):
+    X = tf.placeholder(tf.float32, [n_x, None], name = 'X')
+    Y = tf.placeholder(tf.float32, [n_y, None], name = 'Y')
+    
+    return X, Y
+
+def initialize_parameters(): 
+    W1 = tf.get_variable("W1", [25,12288], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
+    b1 = tf.get_variable("b1", [25,1], initializer = tf.zeros_initializer())
+    W2 = tf.get_variable("W2", [12,25], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
+    b2 = tf.get_variable("b2", [12,1], initializer = tf.zeros_initializer())
+    W3 = tf.get_variable("W3", [6,12], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
+    b3 = tf.get_variable("b3", [6,1], initializer = tf.zeros_initializer())
+
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2,
+                  "W3": W3,
+                  "b3": b3}
+    
+    return parameters
+
+def forward_propagation(X, parameters):
+    W1 = parameters['W1']
+    b1 = parameters['b1']
+    W2 = parameters['W2']
+    b2 = parameters['b2']
+    W3 = parameters['W3']
+    b3 = parameters['b3']
+    
+    Z1 = tf.add(tf.matmul(W1, X), b1) 
+    A1 = tf.nn.relu(Z1)                                      
+    Z2 = tf.add(tf.matmul(W2, A1), b2)                                  
+    A2 = tf.nn.relu(Z2)                            
+    Z3 = tf.add(tf.matmul(W3, A2), b3)
+    
+    return Z3
