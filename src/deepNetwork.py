f0fe941308229588be79b014237099ac2ba50f62
Raymond Zhou <raymondzhou2019@gmail.com>
2020-06-29 20:47:35 -0400

    Created cost computation

--- src/deepNetwork.py
+++ src/deepNetwork.py
@@ -128,4 +128,10 @@ def forward_propagation(X, parameters):
     
     return Z3
 
+def compute_cost(Z3, Y):
+    logits = tf.transpose(Z3)
+    labels = tf.transpose(Y)
 
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = logits, labels = labels))
+
+    return cost
