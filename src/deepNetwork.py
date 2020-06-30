3e03618c0ccb0cf55d975deb21a48da69eae2199
Raymond Zhou <raymondzhou2019@gmail.com>
2020-06-29 20:46:26 -0400

    Added predict tools

--- src/deepNetwork.py
+++ src/deepNetwork.py
@@ -24,6 +24,60 @@ def convert_to_one_hot(Y, C):
     Y = np.eye(C)[Y.reshape(-1)].T
     return Y
 
+def random_mini_batches(X, Y, mini_batch_size = 64, seed = 0):
+    m = X.shape[1]    
+    mini_batches = []
+    np.random.seed(seed)
+ 
+    permutation = list(np.random.permutation(m))
+    shuffled_X = X[:, permutation]
+    shuffled_Y = Y[:, permutation].reshape((Y.shape[0],m))
+
+    num_complete_minibatches = math.floor(m/mini_batch_size) 
+    for k in range(0, num_complete_minibatches):
+        mini_batch_X = shuffled_X[:, k * mini_batch_size : k * mini_batch_size + mini_batch_size]
+        mini_batch_Y = shuffled_Y[:, k * mini_batch_size : k * mini_batch_size + mini_batch_size]
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+
+    if m % mini_batch_size != 0:
+        mini_batch_X = shuffled_X[:, num_complete_minibatches * mini_batch_size : m]
+        mini_batch_Y = shuffled_Y[:, num_complete_minibatches * mini_batch_size : m]
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+    
+    return mini_batches
+
+def convert_to_one_hot(Y, C):
+    Y = np.eye(C)[Y.reshape(-1)].T
+    return Y
+
+
+def predict(X, parameters):
+    W1 = tf.convert_to_tensor(parameters["W1"])
+    b1 = tf.convert_to_tensor(parameters["b1"])
+    W2 = tf.convert_to_tensor(parameters["W2"])
+    b2 = tf.convert_to_tensor(parameters["b2"])
+    W3 = tf.convert_to_tensor(parameters["W3"])
+    b3 = tf.convert_to_tensor(parameters["b3"])
+    
+    params = {"W1": W1,
+              "b1": b1,
+              "W2": W2,
+              "b2": b2,
+              "W3": W3,
+              "b3": b3}
+    
+    x = tf.placeholder("float", [12288, 1])
+    
+    z3 = forward_propagation_for_predict(x, params)
+    p = tf.argmax(z3)
+    
+    sess = tf.Session()
+    prediction = sess.run(p, feed_dict = {x: X})
+        
+    return prediction
+
 X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
 
 X_train_flatten = X_train_orig.reshape(X_train_orig.shape[0], -1).T
@@ -73,3 +127,5 @@ def forward_propagation(X, parameters):
     Z3 = tf.add(tf.matmul(W3, A2), b3)
     
     return Z3
+
+
