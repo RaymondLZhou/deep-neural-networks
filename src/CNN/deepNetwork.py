2c1acd82f2191ce032afa7cb57f8b6c40e1e06a2
Raymond Zhou <raymondzhou2019@gmail.com>
2020-07-03 23:37:09 -0400

    Refactored data functions

--- src/CNN/deepNetwork.py
+++ src/CNN/deepNetwork.py
@@ -4,41 +4,9 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-import os
-import numpy as np
 import matplotlib.pyplot as plt
 
-def loadData(path):
-    train_dir = os.path.join(path, 'train')
-    validation_dir = os.path.join(path, 'validation')
-
-    train_cats_dir = os.path.join(train_dir, 'cats')
-    train_dogs_dir = os.path.join(train_dir, 'dogs')
-    validation_cats_dir = os.path.join(validation_dir, 'cats')
-    validation_dogs_dir = os.path.join(validation_dir, 'dogs')
-
-    return train_dir, validation_dir, train_cats_dir, train_dogs_dir, validation_cats_dir, validation_dogs_dir
-
-def dataSize(train_cats_dir, train_dogs_dir, validation_cats_dir, validation_dogs_dir):
-    num_cats_tr = len(os.listdir(train_cats_dir))
-    num_dogs_tr = len(os.listdir(train_dogs_dir))
-
-    num_cats_val = len(os.listdir(validation_cats_dir))
-    num_dogs_val = len(os.listdir(validation_dogs_dir))
-
-    total_train = num_cats_tr + num_dogs_tr
-    total_val = num_cats_val + num_dogs_val
-
-    print('Total training cat images:', num_cats_tr)
-    print('Total training dog images:', num_dogs_tr)
-
-    print('Total validation cat images:', num_cats_val)
-    print('Total validation dog images:', num_dogs_val)
-
-    print('Total training images:', total_train)
-    print('Total validation images:', total_val)
-
-    return total_train, total_val
+import processData
 
 def plotImages(images_arr):
     fig, axes = plt.subplots(1, 5, figsize=(20,20))
@@ -108,9 +76,9 @@ def assess(history):
 
 path = ('../../data/cats_and_dogs_filtered')
 
-train_dir, validation_dir, train_cats_dir, train_dogs_dir, validation_cats_dir, validation_dogs_dir = loadData(path)
+train_dir, validation_dir, train_cats_dir, train_dogs_dir, validation_cats_dir, validation_dogs_dir = processData.loadData(path)
 
-total_train, total_val = dataSize(train_cats_dir, train_dogs_dir, validation_cats_dir, validation_dogs_dir)
+total_train, total_val = processData.dataSize(train_cats_dir, train_dogs_dir, validation_cats_dir, validation_dogs_dir)
 
 batch_size = 128
 epochs = 15
